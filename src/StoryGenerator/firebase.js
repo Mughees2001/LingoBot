1f4de0de3ecc5352c722979053b8e69f57af7115
Mughees Ur Rehman <108094512+Mughees2001@users.noreply.github.com>
2024-04-30 19:40:59 +0500

    Update firebase.js

--- src/StoryGenerator/firebase.js
+++ src/StoryGenerator/firebase.js
@@ -1,1 +1,25 @@
+import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
+import { getDatabase } from 'firebase/database';
 
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDUJQJF0doOcYG3QGZ_6POfKiYj_6MJ6V4",
+
+  authDomain: "chatbot-7e166.firebaseapp.com",
+
+  projectId: "chatbot-7e166",
+
+  storageBucket: "chatbot-7e166.appspot.com",
+
+  messagingSenderId: "838695247574",
+
+  appId: "1:838695247574:web:b7bd8174add07b777b7377",
+  measurementId: "G-B7QSC0RY8J"
+};
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+const database = getDatabase(app);
+export { app, storage, database };
